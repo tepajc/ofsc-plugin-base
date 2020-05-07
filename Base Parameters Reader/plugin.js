efc1aa6ca49f2b2f7eaf70fc932d71da4abc9370
Miguel Gall Martin <gall.miquel>
2020-05-07 17:02:11 -0400

    First version of Parameters reader

---
+++ Base Parameters Reader/plugin.js
@@ -0,0 +1,117 @@
+"use strict";
+
+var activity;
+
+function openMessage(data) {
+  <!--PLACEHOLDER FOR CUSTOM CODE-- >
+
+  var element = document.getElementById("received-data");
+  element.innerHTML = "<pre>" + JSON.stringify(data, undefined, 4) + "</pre>";
+
+  var result = data.activity.A_OIA_RESULT_INPUT;
+  var dispComm = data.activity.A_DISP_COMMENTS_INPUT;
+
+  var activityData = {
+    "DISPATCHER_COMMENTS": dispComm,
+    "XA_OIA_RESULT" :result,
+		"aid" : data.activity.aid
+  };
+
+  document.getElementById("submit").addEventListener("click", function() {
+		closePlugin(activityData);
+  });
+
+  <!--END OF PLACEHOLDER-- >
+}
+
+function initMessage(data) {
+
+  var messsageData = {
+    apiVersion: 1,
+    method: 'initEnd'
+  };
+
+  sendWebMessage(messsageData);
+}
+
+function initPlugin() {
+
+  window.addEventListener("message", getWebMessage, false);
+
+  var messsageData = {
+    apiVersion: 1,
+    method: 'ready'
+  };
+
+  sendWebMessage(messsageData);
+}
+
+function _isJson(str) {
+  try {
+    JSON.parse(str);
+  } catch (e) {
+    return false;
+  }
+  return true;
+}
+
+function getWebMessage(event) {
+
+  if (typeof event.data === 'undefined') {
+    return false;
+  }
+
+  if (!_isJson(event.data)) {
+    return false;
+  }
+
+  var data = JSON.parse(event.data);
+
+  if (!data.method) {
+    return false;
+  }
+
+  switch (data.method) {
+    case 'open':
+      openMessage(data);
+      break;
+    case 'open':
+      initMessage(data);
+      break;
+    case 'error':
+			console.log("Received error message " + JSON.stringify(data, undefined, 4));
+      break;
+      // other methods may go here
+  }
+}
+
+function getOriginURL(url) {
+  if (url != '') {
+    if (url.indexOf("://") > -1) {
+      return 'https://' + url.split('/')[2];
+    } else {
+      return 'https://' + url.split('/')[0];
+    }
+  }
+  return '';
+}
+
+
+function closePlugin(activityData) {
+
+  var messageData = {
+    "apiVersion": 1,
+    "method": "close",
+    "activity": activityData
+  };
+	console.log("Sending close message" + JSON.stringify(messageData, undefined, 4));
+  sendWebMessage(messageData);
+}
+
+function sendWebMessage(data) {
+  var originUrl = document.referrer || (document.location.ancestorOrigins && document.location.ancestorOrigins[0]) || '';
+
+  if (originUrl) {
+    parent.postMessage(data, getOriginURL(originUrl));
+  }
+}
